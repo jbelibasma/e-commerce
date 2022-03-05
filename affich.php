440538706a46ca4326ba4672e98f6ac0be050ed3
jbelibasma <jbelibasma22@gmail.com>
2022-03-05 22:11:41 +0100

    add panier

--- affich.php
+++ affich.php
@@ -2,7 +2,7 @@
 session_start();
 $dbh = new PDO('mysql:host=localhost;dbname=eshop',
 'root','');
-$query2 = $dbh->prepare(' SELECT `nom`,`quantity`,`products`.`prix` as price,`total` FROM `orderlines`  
+$query2 = $dbh->prepare(' SELECT `nom`,`quantity`,(`products`.`prix`) as price,`total` FROM `orderlines`  
 INNER JOIN `products` ON `products`.`id`=`orderlines`.`product_id` 
 INNER JOIN `orders` on `orders`.`id`=`orderlines`.`order_id` WHERE `order_id` =? ');
 $query2->execute([$_GET['new_sale']]);
